912a0c76b23683f79af4267eedabe8f9a94657bf
George Korsnick <gkorsnick@bolts-Mac.local>
2013-08-31 23:41:31 -0700

    Added STS Specific Information

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,6 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+class sts::java {
+  package { 'STS':
+    provider => 'compressed_app',
+    source   => 'http://download.springsource.com/release/STS/3.3.0/dist/e4.3/spring-tool-suite-3.3.0.RELEASE-e4.3-macosx-cocoa-x86_64.tar.gz'
+  }
 }
--- spec/classes/template_spec.rb
+++ spec/classes/template_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
-# Rename this file to classname_spec.rb
-# Check other boxen modules for examples
-# or read http://rspec-puppet.com/tutorial/
-describe 'template' do
+
+describe 'sts::java' do
   it do
-    should contain_anchor('Hello_World')
+    should contain_package('STS').with({
+      :provider => 'compressed_app',
+      :source   => 'http://download.springsource.com/release/STS/3.3.0/dist/e4.3/spring-tool-suite-3.3.0.RELEASE-e4.3-macosx-cocoa-x86_64.tar.gz'
+    })
   end
 end
